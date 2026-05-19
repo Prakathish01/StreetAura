780275220ae2bfd1a16a81705e8568eb1a866a69
Prakathishwaran <prakathishwaran.manivasagam@psiog.com>
2026-05-19 12:17:42 +0530

    Made some changes

---
+++ js/firebase-env.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA5R2wJXYSkETPZmf9_Fjb9i3UYBMHGFH4",
+  authDomain: "shoppingmart-526a1.firebaseapp.com",
+  projectId: "shoppingmart-526a1",
+  storageBucket: "shoppingmart-526a1.firebasestorage.app",
+  messagingSenderId: "946797765597",
+  appId: "1:946797765597:web:054b702d0489c6b1680c34",
+  measurementId: "G-SWNSFKFK3C"
+};
